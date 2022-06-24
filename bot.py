03253a811ccd1884a18a08de1b492abdd8cdc95b
Heptor44 <heptor42@gmail.com>
2022-06-23 22:16:41 -0500

    switch to keyboard and mouse modules, add lerp

--- bot.py
+++ bot.py
@@ -1,69 +1,87 @@
 import time
-import winreg
-import sys
-import os
+import json
+
+from pathlib import Path
 
 try:
-	import win32api, win32con
+	import mouse, keyboard
 except ModuleNotFoundError:
-	print('Modules win32api and win32con not found. Did you forget to [pip install -r requirements.txt]?')
+	print('Mouse and keyboard modules not found. Did you forget to [pip install -r requirements.txt]?')
+	exit(1)
+except ImportError:
+	print('Sorry, you have to be on root to use this on Linux due to a limitation with the keyboard and mouse modules.')
 	exit(1)
 
 def main():
-	print('SSPlusBot v0.1.0')
-	reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-	with winreg.OpenKeyEx(reg, 'Control Panel\Mouse', 0, winreg.KEY_READ) as regkey:
-		print('Read-only access gained to registry key HKEY_CURRENT_USER\Control Panel\Mouse')
-		if not winreg.QueryValueEx(regkey,'MouseSpeed')[0]:
-			sys.stderr.write('''"Enhance pointer precision" is currently on in mouse settings,
-which makes this unusable. 
-Please turn it off and reopen.''')
-			os.system('start ms-settings:mousetouchpad')
-			return
-		speed = int(winreg.QueryValueEx(regkey,'MouseSensitivity')[0])
-		# i made a data table and this seems to be piecewise? i may be wrong
-		if speed < 5:
-			ratio = (2**(6-speed))
-			d_ratio = f'2^{6-speed}'
-		elif 5 <= speed < 9:
-			ratio = (12-speed)/3
-			d_ratio = f'{12-speed}/3'
-		else:
-			ratio = 2/((speed/2))
-			d_ratio = f'2/{int(speed/2) if int(speed/2) == speed/2 else speed/2}'
-		print(f'Calculated inverse of mouse sensitivity ratio for raw pixel movement: {d_ratio} ({ratio:.6f})')
-	def move_to_note(note,old_note):
-		win32api.mouse_event(win32con.MOUSEEVENTF_MOVE,int(((2-note[0])-(2-old_note[0]))*200*ratio),int(((2-note[1])-(2-old_note[1]))*200*ratio))
-	song = [[float(n) for n in note.split('|')] for note in input('Paste song data string here:\n').split(',')[1:]]
-	print('Song loaded, press F7 to start')
-	while not win32api.GetAsyncKeyState(win32con.VK_F7):
-		time.sleep(0)
-	old_time = time.time()
+	print('SSPlusBot v3')
+	if Path('./config.json').exists():
+		try:
+			with open('./config.json','r') as config_file:
+				config = json.load(config_file)
+				center = config['center']
+			i = input('Would you like to recalibrate?\n[Y] for yes, [Any else] for no\n')
+			do_config = i.lower() == 'y'
+		except:
+			print('Config file invalid, recalibrating...')
+			do_config = True
+	else:
+		print('Config file not found, calibrating...')
+		do_config = True
+	if do_config:
+		print('Move your cursor to the top left of the play field and press F8.')
+		keyboard.wait('f8')
+		top_left = mouse.get_position()
+		print('Move your cursor to the bottom right of the play field and press F8.')
+		keyboard.wait('f8')
+		bottom_right = mouse.get_position()
+		center = [(a+b)//2 for a,b in zip(top_left,bottom_right)]
+		with open('./config.json','w+') as config_file:
+			config = json.dump({'center':center},config_file)
+	def move_to(x,y):
+		x, y = ((1-x)*55.3333333333)+center[0], ((1-y)*55.3333333333)+center[1]
+		mouse.move(x,y)
+	i = None
+	while i not in ['1','2']:
+		i = input('Input a song with:\n[1] Data file path [.txt]\n[2] Paste in data\n')
+	match i:
+		case '1':
+			while 1:
+				i = input('Input file path: ')
+				try:
+					with open(i,'r') as f:
+						song_data = f.read()
+					break
+				except FileNotFoundError:
+					continue
+		case '2':
+			song_data = input('Input song data: ')
+	song = [[float(n) for n in note.split('|')] for note in song_data.split(',')[1:]]
+	print('Song loaded, click play and press F8\nPress F7 when the first note is at the timing window to start')
+	keyboard.wait(66)
+	mouse.move(*center)
+	keyboard.wait(65) #wait for F7
+	old_time = time.perf_counter()
 	offset = song[0][2]
 	print(f'\rOffset set to {offset}ms',end='                     ')
-	move_to_note(song[0],[1.0,1.0,0])
+	move_to(*song[0][:2])
 	old_note = song.pop(0)
 	while len(song):
-		x, y = [], []
-		for note in song[:10]:
-			if (old_time + ((old_note[2]-offset)/1000)) <= (old_time + ((note[2]-offset)/1000)) <= time.time():
-				x.append(note[0])
-				y.append(note[1])
-				timing = song.pop(0)[2]
-		if len(x):
-			move_to_note([sum(x)/len(x),sum(y)/len(y)],old_note)
-			old_note = [sum(x)/len(x),sum(y)/len(y),timing]
-		if win32api.GetAsyncKeyState(win32con.VK_RIGHT):
+		note = song[0]
+		delta = (time.perf_counter()-(old_time + ((old_note[2]-offset)/1000)))/(((note[2]-offset)/1000)-((old_note[2]-offset)/1000))
+		move_to(*[(old*(1-delta))+(new*delta) for old, new in zip(old_note[:2],note[:2])])
+		if delta >= 1:
+			old_note = song.pop(0)
+		if keyboard.is_pressed(77):
 			if kr:
 				kr = False
-				offset += 10 if win32api.GetAsyncKeyState(win32con.VK_SHIFT) else 1
+				offset += 10 if keyboard.is_pressed('shift') else 1
 				print(f'\rOffset set to {offset}ms',end='                     ')
 		else:
 			kr = True
-		if win32api.GetAsyncKeyState(win32con.VK_LEFT):
+		if keyboard.is_pressed(75):
 			if kl:
 				kl = False
-				offset -= 10 if win32api.GetAsyncKeyState(win32con.VK_SHIFT) else 1
+				offset -= 10 if keyboard.is_pressed('shift') else 1
 				print(f'\rOffset set to {offset}ms',end='                     ')
 		else:
 			kl = True
