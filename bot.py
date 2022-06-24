12026c813f0142170df689a7ed580c103b896140
Heptor44 <heptor42@gmail.com>
2022-06-23 23:48:46 -0500

    scrap centering, add support for easing modes, allow stopping song prematurely on pause

--- bot.py
+++ bot.py
@@ -13,31 +13,28 @@ except ImportError:
 	exit(1)
 
 def main():
-	print('SSPlusBot v3')
+	print('''Starting SSBot. Don't use this to fake a score, you won't get away with it.''')
 	if Path('./config.json').exists():
 		try:
 			with open('./config.json','r') as config_file:
 				config = json.load(config_file)
-				center = config['center']
-			i = input('Would you like to recalibrate?\n[Y] for yes, [Any else] for no\n')
+			i = input('Would you like to reset config?\n[Y] for yes, [Not Y] for no\n')
 			do_config = i.lower() == 'y'
 		except:
-			print('Config file invalid, recalibrating...')
+			print('Config file invalid, resetting config...')
 			do_config = True
 	else:
-		print('Config file not found, calibrating...')
+		print('''Config file not found. Assuming this is your first time using SSBot:
+1: In SS+, go to Settings > Camera & Control.
+2: Set your sensitivity to 1.
+3: Uncheck "Lock Mouse".
+
+Now calibrating playfield. Please open a map and pause the game.''')
 		do_config = True
 	if do_config:
-		print('Move your cursor to the top left of the play field and press F8.')
-		keyboard.wait('f8')
-		top_left = mouse.get_position()
-		print('Move your cursor to the bottom right of the play field and press F8.')
-		keyboard.wait('f8')
-		bottom_right = mouse.get_position()
-		center = [(a+b)//2 for a,b in zip(top_left,bottom_right)]
 		with open('./config.json','w+') as config_file:
-			config = json.dump({'center':center},config_file)
-	def move_to(x,y):
+			config = json.dump({'first_time':True},config_file)
+	def move_to(x,y,center):
 		x, y = ((1-x)*55.3333333333)+center[0], ((1-y)*55.3333333333)+center[1]
 		mouse.move(x,y)
 	i = None
@@ -55,20 +52,38 @@ def main():
 					continue
 		case '2':
 			song_data = input('Input song data: ')
-	song = [[float(n) for n in note.split('|')] for note in song_data.split(',')[1:]]
-	print('Song loaded, click play and press F8\nPress F7 when the first note is at the timing window to start')
-	keyboard.wait(66)
-	mouse.move(*center)
+	song_raw = [[float(n) for n in note.split('|')] for note in song_data.split(',')[1:]]
+	song = []
+	notes = []
+	avg = lambda args: sum(args)/len(args)
+	for note in song_raw:
+		if len(notes):
+			if note[2]-notes[0][2] < 10:
+				notes.append(note)
+			else:
+				song.append([avg([note[0] for note in notes]),avg([note[1] for note in notes]),notes[0][2]])
+				notes = [note]
+		else:
+			notes.append(note)
+	if len(notes):
+		song.append([avg([note[0] for note in notes]),avg([note[1] for note in notes]),notes[0][2]])
+	print('Song loaded, click play\nPress F7 when the first note is at the timing window to start')
 	keyboard.wait(65) #wait for F7
+	center = mouse.get_position()
 	old_time = time.perf_counter()
 	offset = song[0][2]
 	print(f'\rOffset set to {offset}ms',end='                     ')
-	move_to(*song[0][:2])
+	move_to(*song[0][:2],center)
 	old_note = song.pop(0)
+	easing = lambda t: (t - 1) * (t - 1) * (t - 1) + 1
 	while len(song):
 		note = song[0]
-		delta = (time.perf_counter()-(old_time + ((old_note[2]-offset)/1000)))/(((note[2]-offset)/1000)-((old_note[2]-offset)/1000))
-		move_to(*[(old*(1-delta))+(new*delta) for old, new in zip(old_note[:2],note[:2])])
+		try:
+			delta = (time.perf_counter()-(old_time + ((old_note[2]-offset)/1000)))/(((note[2]-offset)/1000)-((old_note[2]-offset)/1000))
+			delta = easing(delta)
+		except ZeroDivisionError:
+			delta = 1
+		move_to(*[(old*(1-delta))+(new*delta) for old, new in zip(old_note[:2],note[:2])],center)
 		if delta >= 1:
 			old_note = song.pop(0)
 		if keyboard.is_pressed(77):
@@ -85,6 +100,9 @@ def main():
 				print(f'\rOffset set to {offset}ms',end='                     ')
 		else:
 			kl = True
+		if keyboard.is_pressed(57) or keyboard.is_pressed(1):
+			print('\n[!] Song stopped prematurely.')
+			break
 	print('\nSong finished!')
 
 
